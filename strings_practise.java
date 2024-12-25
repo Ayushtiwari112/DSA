29f03d5636ced218fac8860286164e0670821253
Ayushtiwari112 <143792046+Ayushtiwari112@users.noreply.github.com>
2024-12-25 14:54:07 +0530

    Add files via upload

---
+++ strings_practise.java
@@ -0,0 +1,46 @@
+package dsa.src;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class strings_practise {
+    public static void main(String[] args){
+
+        // Lower case vowels counting
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a Word or Line");
+//        String s = sc.nextLine();
+//        int count = 0;
+//        for (int i=0;i<s.length();i++){
+//            char ch = s.charAt(i);
+//            if (ch=='a' || ch=='e'  ||ch=='i'  || ch=='o'  || ch=='u'){
+//                count++;
+//            }
+//        }
+//        System.out.println("Lower case Vowels: " + count);
+        // TC=O(n)
+
+
+        //.equals
+//        String str = "AyushTiwari";
+//        String str1 = "TiwariAyush";
+//        String st2 = "AyushTiwari";
+//        System.out.println(str.equals(st2));
+
+
+        //.replace
+//        String str = "AyushTiwari";
+//        str = str.replace('h', 'a');
+//        System.out.println(str);
+
+
+        //Anagrams (two strings with same characters but in different order)
+//        String str = "Ayush";
+//        String str1 = "yuAsh";
+//        char[] strArr = str.toCharArray(); //['a','y','u','s','h']
+//        char[] str1Arr = str1.toCharArray();
+//        Arrays.sort(strArr); // arrange in ascending order
+//        Arrays.sort(str1Arr);
+//        System.out.println(Arrays.equals(strArr,str1Arr));
+    }
+}
